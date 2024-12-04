e25362d5ff69afa3a2cef00205b4e711aaa257af
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-04 18:59:06 +0530

    Add: Logic for Snake, Ladder, and No Play options

--- src/SnakeAndLadderGame.java
+++ src/SnakeAndLadderGame.java
@@ -9,5 +9,20 @@ public class SnakeAndLadderGame {
         Random random = new Random();
         int dieRoll = random.nextInt(6) + 1; // Rolling a die (1-6)
         System.out.println("Player rolled a: " + dieRoll);
+        int option = random.nextInt(3); // 0: No Play, 1: Ladder, 2: Snake
+        switch (option) {
+            case 0:
+                System.out.println("No Play: Player stays at the same position.");
+                break;
+            case 1:
+                playerPosition += dieRoll;
+                System.out.println("Ladder: Player moves to position " + playerPosition);
+                break;
+            case 2:
+                playerPosition -= dieRoll;
+                if (playerPosition < 0) playerPosition = 0;
+                System.out.println("Snake: Player moves to position " + playerPosition);
+                break;
+        }
     }
 }
