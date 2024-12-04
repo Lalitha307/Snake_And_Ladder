7e21d12125944abf1d6978fd2fef51cdd353bdf0
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-04 19:10:29 +0530

    Add: Report dice rolls and player position after each roll

--- src/SnakeAndLadderGame.java
+++ src/SnakeAndLadderGame.java
@@ -3,27 +3,29 @@ public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Simulator!");
         int playerPosition = 0;
+        int diceRollCount = 0;
         Random random = new Random();
         while (playerPosition < 100) {
+            diceRollCount++;
             int dieRoll = random.nextInt(6) + 1;
             int option = random.nextInt(3);
             switch (option) {
                 case 0: // No Play
+                    System.out.println("Dice Roll " + diceRollCount + ": No Play, Player remains at " + playerPosition);
                     break;
                 case 1: // Ladder
                     if (playerPosition + dieRoll <= 100) {
                         playerPosition += dieRoll;
                     }
+                    System.out.println("Dice Roll " + diceRollCount + ": Ladder, Player moves to " + playerPosition);
                     break;
                 case 2: // Snake
                     playerPosition -= dieRoll;
                     if (playerPosition < 0) playerPosition = 0;
+                    System.out.println("Dice Roll " + diceRollCount + ": Snake, Player moves to " + playerPosition);
                     break;
             }
-
-            System.out.println("Player is at position: " + playerPosition);
         }
-
-        System.out.println("Player wins the game by reaching position 100!");
+        System.out.println("Player wins the game in " + diceRollCount + " rolls!");
     }
 }
