d873a220be78c278615e1a51a3177bad932b1f03
Lalitha307 <21A91A05D2@aec.edu.in>
2024-12-04 18:56:21 +0530

    Add: Player starting position logic

--- src/SnakeAndLadderGame.java
+++ src/SnakeAndLadderGame.java
@@ -3,5 +3,7 @@
 public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Simulator!");
+        int playerPosition = 0;
+        System.out.println("Player starts at position: " + playerPosition);
     }
 }
